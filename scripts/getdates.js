3fb6cac65f6823bf46a6d663554d066ad939e622
sankysacre <sankysacre0@gmail.com>
2026-09-01 03:47:16 -0700

    add

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const currentYearSpan = document.getElementById("currentyear");
+if (currentYearSpan) {
+    currentYearSpan.textContent = new Date().getFullYear();
+}
+
+const lastModifiedParagraph = document.getElementById("lastModified");
+if (lastModifiedParagraph) {
+    lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+}
